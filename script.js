8791afbace3f5cf5deab7e06820c4911f3241962
Yujing <subechen@gmail.com>
2026-06-17 12:29:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,22 @@ function breatheLight() {
 }
 
 breatheLight();
+
+const revealElements = document.querySelectorAll(".reveal");
+
+const revealObserver = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.16
+  }
+);
+
+revealElements.forEach(element => {
+  revealObserver.observe(element);
+});
